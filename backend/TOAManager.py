23265c4328b439fe2a982dc2262008e4f6fdb179
Steve-Cruz7 <sacruz7@miners.utep.edu>
2024-03-22 17:08:22 -0600

    Updated EventRepresenter and LogIngestor with TOA info

--- backend/TOAManager.py
+++ backend/TOAManager.py
@@ -1,4 +1,12 @@
 class TOAManager:
     def __init__(self):
-        self.x = 0
+        self.libary = "Some File path we don't have yet"
+    
+    def setTOA(self, team):
+        if team == 'blue':
+            return 'blueDefault.png', 'blueDefault'
+        elif team == 'white':
+            return 'whiteDefault.png', 'whiteDefault'
+        else:
+            return 'redDefault.png', 'redDefault'
     
