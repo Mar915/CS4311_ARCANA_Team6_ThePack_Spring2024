de71ae7da5a8e2d477c6236ffde31359c11d87d9
AleLeeHrtz <ale@MacBook-Air-de-Alejandra.local>
2024-03-29 19:55:59 -0600

    Added updateAdjList method

--- backend/EventGraphManager.py
+++ backend/EventGraphManager.py
@@ -6,6 +6,7 @@ class EventGraphManager:
         self.db = db
         self.projName = projName
         self.eventList = EventsManager.pullEvents()
+
     
     def updatePosition(self, newData):
         eventsDB = self.db['projectRepList'][self.projName]['eventRepList']
@@ -19,4 +20,17 @@ class EventGraphManager:
             changes['yCord'] = newData['yCord']
         
         newValues = {'$set' : changes}
+        eventsDB.update_one(query, newValues)
+
+
+    def updateAdjList(self, newData):
+        eventsDB = self.db['projectRepList'][self.projName]['eventRepList']
+        targetEvent = newData['currEvent']
+        query = {'id': targetEvent['id']}
+        changes = {}
+
+        if newData['AdjList'] != '':
+            changes['AdjList'] = newData['AdjList']
+
+        newValues = {'$set': changes}
         eventsDB.update_one(query, newValues)
